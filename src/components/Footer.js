6d6388323ae49910375ca6667c1be25052a6c440
gersonbarajas62 <gersonbarajas62@gmail.com>
2023-05-08 12:43:02 -0600

    Skills added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,7 +16,8 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="https://www.linkedin.com/in/gerson-roa/" target="_blank" > <img src={navIcon1} alt="Icon" /></a>
+              <a href="https://www.linkedin.c
+              om/in/gerson-roa/" target="_blank"> <img src={navIcon1} alt="Icon" /></a>
               <a href="#"><img src={navIcon2} alt="Icon" /></a>
               <a href="#"><img src={navIcon3} alt="Icon" /></a>
             </div>
